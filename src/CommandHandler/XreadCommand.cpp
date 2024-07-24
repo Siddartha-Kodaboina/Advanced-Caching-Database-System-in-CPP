261497232ae125574e2712edbe0a0f0f45306c72
Siddartha Kodaboina <k.siddartha49@gmail.com>
2024-07-24 16:00:44 -0700

    Xread: Altered xreadCommand to fetch multiple streams

--- src/CommandHandler/XreadCommand.cpp
+++ src/CommandHandler/XreadCommand.cpp
@@ -7,31 +7,52 @@ XreadCommand::XreadCommand() {}
 
 std::vector<std::string> XreadCommand::execute(const std::vector<std::string>& args) {
     std::cout << "XrangeCommand was it here?:\n-----------" << std::endl;
-    if (args.size() == 4 ) {
-        std::string streamKey = args[2];
-        std::string id = args[3];
-        std::cout << "XaddCommand was it here?:\n++++++" << std::endl;
-        auto entries = KeyValueStore::getInstance().xread(streamKey, id);
-        return formatForRESP(entries, streamKey);
-        
+    if (args.size() >= 4 && args.size()%2==0 ) {
+      std::vector<std::string> streamKeys = {};
+      std::vector<std::string> ids = {};
+      int lenOfStreamIDs = args.size()-2;
+      for(int i = 0; i < lenOfStreamIDs;i++){
+        if(i<(lenOfStreamIDs/2)){
+          streamKeys.push_back(args[2+i]);
+        }else{
+          ids.push_back(args[2+i]);
+        }
+      }
+      std::vector<std::pair<Stream, std::string>> streams = {};
+      for(int i=0; i<streamKeys.size();i++){
+        std::string streamKey = streamKeys[i];
+        std::string id = ids[i];
+        std::cout << "Stream is " << streamKey << std::endl;
+        std::cout << "id is " << id << std::endl;
+        Stream stream = KeyValueStore::getInstance().xread(streamKey, id);
+        streams.push_back({stream, streamKey});
+      }
+      return formatForRESP(streams);
+      
     }
     return {"-ERR wrong number of arguments"};
 }
 
-std::vector<std::string> XreadCommand::formatForRESP(const Stream& entries, std::string streamKey) {
+std::vector<std::string> XreadCommand::formatForRESP(const std::vector<std::pair<Stream, std::string>>& streams) {
     std::vector<std::string> formattedEntries;
-    formattedEntries.push_back("*" + std::to_string(1));
-    formattedEntries.push_back("*" + std::to_string(2));
-    formattedEntries.push_back("$" + streamKey);
-    formattedEntries.push_back("*" + std::to_string(entries.size())); // Size of the array
-    for (const auto& entry : entries) {
-        formattedEntries.push_back("*2"); // Each entry has 2 elements (ID and array of fields)
-        formattedEntries.push_back("$" + entry.id); // Stream entry ID
-        formattedEntries.push_back("*" + std::to_string(entry.fields.size() * 2)); // Number of fields * 2 (field name and value)
-        for (const auto& field : entry.fields) {
-            formattedEntries.push_back("$" + field.first); // Field name
-            formattedEntries.push_back("$" + field.second); // Field value
-        }
+    formattedEntries.push_back("*" + std::to_string(streams.size()));
+    for(const auto& streamPair : streams){
+      Stream stream = streamPair.first;
+      std::string streamKey = streamPair.second;
+      
+      formattedEntries.push_back("*" + std::to_string(2));
+      formattedEntries.push_back("$" + streamKey);
+      formattedEntries.push_back("*" + std::to_string(stream.size())); // Size of the array
+      for (const auto& entry : stream) {
+          formattedEntries.push_back("*2"); // Each entry has 2 elements (ID and array of fields)
+          formattedEntries.push_back("$" + entry.id); // Stream entry ID
+          formattedEntries.push_back("*" + std::to_string(entry.fields.size() * 2)); // Number of fields * 2 (field name and value)
+          for (const auto& field : entry.fields) {
+              formattedEntries.push_back("$" + field.first); // Field name
+              formattedEntries.push_back("$" + field.second); // Field value
+          }
+      }
     }
+    
     return formattedEntries;
 }
--- src/CommandHandler/XreadCommand.hpp
+++ src/CommandHandler/XreadCommand.hpp
@@ -7,5 +7,5 @@ class XreadCommand : public ICommand {
 public:
     XreadCommand();
     std::vector<std::string> execute(const std::vector<std::string>& args) override;
-    std::vector<std::string> formatForRESP(const Stream& entries,  std::string streamKey);
+    std::vector<std::string> formatForRESP(const std::vector<std::pair<Stream, std::string>>& entries);
 };
