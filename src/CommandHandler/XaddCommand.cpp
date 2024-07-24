4cdb78b9fede5175eaf3828924ac19f15cc2bbdb
Siddartha Kodaboina <k.siddartha49@gmail.com>
2024-07-24 15:16:00 -0700

    Adding xread to redis server

--- src/CommandHandler/XaddCommand.cpp
+++ src/CommandHandler/XaddCommand.cpp
@@ -1,6 +1,5 @@
 #include "XaddCommand.hpp"
 #include <iostream>
-#include <sstream>
 #include "../Store/KeyValueStore.hpp"
 
 XaddCommand::XaddCommand() {}
@@ -8,23 +7,13 @@ XaddCommand::XaddCommand() {}
 std::vector<std::string> XaddCommand::execute(const std::vector<std::string>& args) {
     std::cout << "XaddCommand was it here?:\n-----------" << std::endl;
     std::cout << "size: " <<args.size() << std::endl;
-    if (args.size() == 4) {
+    if (args.size() >= 5) {
         std::string streamKey = args[1];
         std::string id = args[2];
-
-        std::istringstream iss(args[3]);
-        std::vector<std::string> tokens;
-        std::string token;
-        while (iss >> token) {
-            tokens.push_back(token);
-        }
-
+        
         std::unordered_map<std::string, std::string> fieldValues;
-        for (size_t i = 0; i < tokens.size(); i += 2) {
-            fieldValues[tokens[i]] = tokens[i + 1];
-        }
-        for(const auto& tokenPair: fieldValues) {
-            std::cout << tokenPair.first << " " << tokenPair.second << std::endl;
+        for (size_t i = 3; i < args.size(); i += 2) {
+            fieldValues[args[i]] = args[i + 1];
         }
         std::cout << "XaddCommand was it here?:\n++++++" << std::endl;
         return {KeyValueStore::getInstance().xadd(streamKey, id, fieldValues)};
